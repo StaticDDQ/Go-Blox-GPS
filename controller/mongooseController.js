829261f75967115f679a055e2f90f7033e3b6d02
Nicola Halim <staticddq@gmail.com>
2019-04-10 15:23:46 +1000

    Update mongooseController.js
    moved schema from schemacontroller to mongoosecontroller

--- controller/mongooseController.js
+++ controller/mongooseController.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
-const schema = require('./schemaController');
+//const schema = require('./schemaController');
+const MongoClient = require('mongodb').MongoClient;
+
+var schema = new mongoose.Schema({
+    "firstName": String,
+    "lastName": String,
+    "userName": String,
+    "email": String,
+    "DOB": Date,
+    "password": String
+});
 
 var data = {
     username: "test",
@@ -15,15 +25,22 @@ var stringMongoose = "mongodb://" + data.username + ":" + data.password + "@ds23
 mongoose.connect(stringMongoose, options)
     .catch((err) => console.log("ERORR:" + err));
 
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function (callback) {
-    console.log("connection succeeded");
-});
-
 var Members = mongoose.model("members", schema);
 
-exports.addUser = (req,res) => {
+MongoClient.connect(stringMongoose, function (err, db) {
+    if (err) throw err;
+
+    var dbo = db.db("members_goblox");
+    dbo.collection("members").findOne({}, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+});
+
+exports.addUser = (req, res) => {
+
+    //console.log(req.body);
     var data = new Members({
         "firstName": req.body.firstName,
         "lastName": req.body.lastName,
@@ -32,9 +49,12 @@ exports.addUser = (req,res) => {
         "password": req.body.password,
         "email": req.body.email
     });
+    console.log(data.firstName);
 
     data.save(function (err, newMember) {
         if (!err) {
+            
+            //console.log(Members.find());
             res.send(newMember);
         } else {
             res.sendStatus(400);
