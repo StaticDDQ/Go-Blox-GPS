92f60c3461cb8551a7d0247e9b9376b9f806612a
Nicola Halim <staticddq@gmail.com>
2019-04-15 13:56:43 +1000

    Update app.js
    add proper directory to html file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const database = require('./db');
 const mongooseController = require('./controller/mongooseController.js');
 const bodyParser = require('body-parser');
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', function (req, res) {
-    res.sendFile('home.html');
+    res.sendFile(path.join(__dirname+ '/home.html'));
 });
 
 /***************************  MEMBERS  ***************************/
