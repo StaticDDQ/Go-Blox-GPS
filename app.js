be57c95e36b23bbad493e459312b44eac9452950
Nicola Halim <staticddq@gmail.com>
2019-04-15 13:51:14 +1000

    temporary move home.html

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/home.html');
+    res.sendFile('/home.html');
 });
 
 /***************************  MEMBERS  ***************************/
