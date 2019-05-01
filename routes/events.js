abf64b03b864c47142d472b8019c3ba2dca91c5f
Raisa <ramalia@student.unimelb.edu.au>
2019-05-01 17:18:46 +1000

    all jansen

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 // Get event model
 let Event = require('../models/event');
