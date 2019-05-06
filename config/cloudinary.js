2c0b1ce65942676cc4ff8d173f7c7d7d2b76b7a9
Jansen Kwong <j.kwong4@student.unimelb.edu.au>
2019-05-06 13:31:59 +1000

    add events complete

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,6 @@
 const cloudinary = require("cloudinary");
 
-exports.cloudinary.config({ 
+cloudinary.config({ 
     cloud_name: 'dqcp78pjg', 
     api_key: '936829188533569', 
     api_secret: 'pHCrabaDxHQA1tzgLaKpBol1Y6M' 
